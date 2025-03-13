825498d512b869c137088294475c34b371ae6717
YanisGuettouf <yanis.guettouf@etu.univ-lyon1.fr>
2025-03-13 09:58:37 +0100

    F

--- ProjetRezo/src/UDPClient.java
+++ ProjetRezo/src/UDPClient.java
@@ -4,17 +4,30 @@ import java.net.InetAddress;
 
 public class UDPClient {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         String serverIP = "192.168.104.166"; // Adresse IP du serveur
+=======
+        String serverIP = "192.168.104.166"; // Adresse du serveur
+>>>>>>> Stashed changes
         int serverPort = 12345; // Port du serveur
 
         try (DatagramSocket clientSocket = new DatagramSocket()) { // Création du socket client
             // Préparer le message à envoyer
+<<<<<<< Updated upstream
             String message = "MEW MEW MEW MEW "; // Message envoyé au serveur
+=======
+            String message = "Bonjour ";
+>>>>>>> Stashed changes
             byte[] sendData = message.getBytes();
             InetAddress serverAddress = InetAddress.getByName(serverIP); // Convertit l'adresse en objet InetAddress
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+<<<<<<< Updated upstream
 
             // Envoyer le message au serveur
+=======
+😂
+            // Envoyer le message
+>>>>>>> Stashed changes
             clientSocket.send(sendPacket);
             System.out.println("Message envoyé au serveur : " + message);
 
